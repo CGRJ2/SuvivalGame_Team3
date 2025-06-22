9729c0114bf95904e611babeafd1b3d49932ebc7
mjmin99 <mjmin99@naver.com>
2025-06-22 18:14:39 +0900

    [Feat] 아이템의 습득, 감지
    - Scriptable Object 로 아이템 제작, 양산 가능
    - 캐릭터 시점의 메인 카메라로 부터 일정 거리의 아이템을 감지해내어 텍스트 출력
    - 상호작용키(현재는 임시로 E 키에 배정) 누를 시 습득

--- Assets/00WorkSpace/MMJ/Test_1/ItemPickUp.cs
+++ Assets/00WorkSpace/MMJ/Test_1/ItemPickUp.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EquipmentType
+public class ItemPickUp : MonoBehaviour
 {
-    Weapon,
-    Armor,
-    Accessory
+    public Item Item;
 }
