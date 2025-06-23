88ce062258275d6ddeaac04be74a9fd979eaaf98
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-06-23 10:10:28 +0900

    [Docs] ItemInstance 클래스 이름 변경

--- Assets/00WorkSpace/MMJ/Test_1/ItemInstance.cs
+++ Assets/00WorkSpace/MMJ/Test_1/ItemInstance.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickUp : MonoBehaviour
+public class ItemInstance : MonoBehaviour
 {
     public Item Item;
 }
