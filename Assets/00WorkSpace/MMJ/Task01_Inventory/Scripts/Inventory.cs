75bff8ef122f16ac415429ae722bff5291d91b2e
mjmin99 <mjmin99@naver.com>
2025-06-24 17:48:58 +0900

    [Feat] 탭 구조 인벤토리 구현
    - 탭 구조 인벤토리를 구현했다.
    - 하지만 슬롯별로 들어갈 수 있는 아이템 판정이 되어있지 않아서 탭과 관계없는 아이템을 옮길 수 있다.
    - 커밋을 남기는 이유는 아이디어가 생겼는데 이 시스템으로 탭을 구분하기보단 서바이벌 장르에서 가방이 추가로 생기는 그런 시스템을 구현 할 수 있지 않을까? 하는 생각이 들었다

--- Assets/00WorkSpace/MMJ/Task01_Inventory/Scripts/Inventory.cs
+++ Assets/00WorkSpace/MMJ/Task01_Inventory/Scripts/Inventory.cs
@@ -1,35 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Item;
 
 public class Inventory : MonoBehaviour
 {
     public static bool inventoryActivated = false;
 
-    //필요한 컴포넌트
-    [SerializeField]
-    private GameObject go_inventoryBase;
-    [SerializeField]
-    private GameObject go_SlotsParent;
-    [SerializeField]
-    private GameObject go_QuickSlotParent;
+    [SerializeField] private GameObject go_inventoryBase;
+    [SerializeField] private GameObject go_QuickSlotParent;
 
-    [SerializeField]
-    private GameObject go_Base; //Base_Outer 참조
-    
+    [SerializeField] private GameObject go_EquipmentSlotsParent;
+    [SerializeField] private GameObject go_UsedSlotsParent;
+    [SerializeField] private GameObject go_IngredientSlotsParent;
+    [SerializeField] private GameObject go_FunctionSlotsParent;
+    [SerializeField] private GameObject go_QuestSlotsParent;
 
-    //인벤토리 슬롯들
-    private Slot[] slots;
-    //퀵슬롯들
+    [SerializeField] private GameObject go_Base; // Tooltip Base_Outer
+
+    private Slot[] equipmentSlots;
+    private Slot[] usedSlots;
+    private Slot[] ingredientSlots;
+    private Slot[] functionSlots;
+    private Slot[] questSlots;
     private Slot[] quickSlots;
+
     private bool isNotPut;
 
+    private ItemType currentTab = ItemType.Equipment; // 기본 무기 탭
 
     private void Start()
     {
-        
-        slots = go_SlotsParent.GetComponentsInChildren<Slot>();
+        equipmentSlots = go_EquipmentSlotsParent.GetComponentsInChildren<Slot>();
+        usedSlots = go_UsedSlotsParent.GetComponentsInChildren<Slot>();
+        ingredientSlots = go_IngredientSlotsParent.GetComponentsInChildren<Slot>();
+        functionSlots = go_FunctionSlotsParent.GetComponentsInChildren<Slot>();
+        questSlots = go_QuestSlotsParent.GetComponentsInChildren<Slot>();
         quickSlots = go_QuickSlotParent.GetComponentsInChildren<Slot>();
+
+        ChangeTab(ItemType.Equipment); // 초기 무기탭
     }
 
     private void Update()
@@ -44,13 +53,9 @@ public class Inventory : MonoBehaviour
             inventoryActivated = !inventoryActivated;
 
             if (inventoryActivated)
-            {
                 OpenInventory();
-            }
             else
-            {
                 CloseInventory();
-            }
         }
     }
 
@@ -58,45 +63,77 @@ public class Inventory : MonoBehaviour
     {
         go_inventoryBase.SetActive(true);
     }
+
     private void CloseInventory()
     {
         go_inventoryBase.SetActive(false);
-        go_Base.SetActive(false); //툴팁을 띄운채로 인벤토리를 닫으면 툴팁이 유지되는 버그가 있어서 수정한 내용 
+        go_Base.SetActive(false);
+    }
+
+    public void ChangeTab(ItemType type)
+    {
+        currentTab = type;
+
+        go_EquipmentSlotsParent.SetActive(false);
+        go_UsedSlotsParent.SetActive(false);
+        go_IngredientSlotsParent.SetActive(false);
+        go_FunctionSlotsParent.SetActive(false);
+        go_QuestSlotsParent.SetActive(false);
+
+        switch (currentTab)
+        {
+            case ItemType.Equipment: go_EquipmentSlotsParent.SetActive(true); break;
+            case ItemType.Used: go_UsedSlotsParent.SetActive(true); break;
+            case ItemType.Ingredient: go_IngredientSlotsParent.SetActive(true); break;
+            case ItemType.Function: go_FunctionSlotsParent.SetActive(true); break;
+            case ItemType.Quest: go_QuestSlotsParent.SetActive(true); break;
+        }
     }
 
     public void AcquireItem(Item _item, int _count = 1)
     {
-        PutSlot(quickSlots, _item, _count);
+        PutSlot(quickSlots, _item, _count); // 퀵슬롯 먼저 시도
         if (isNotPut)
         {
-            PutSlot(slots, _item, _count);
+            Slot[] targetSlots = GetTargetSlotArray(_item.itemType);
+            PutSlot(targetSlots, _item, _count);
         }
 
         if (isNotPut)
-            Debug.Log("인벤토리가 꽉찼옹~");
+            Debug.Log("인벤토리가 꽉찼습니다!");
+    }
 
+    private Slot[] GetTargetSlotArray(ItemType type)
+    {
+        switch (type)
+        {
+            case ItemType.Equipment: return equipmentSlots;
+            case ItemType.Used: return usedSlots;
+            case ItemType.Ingredient: return ingredientSlots;
+            case ItemType.Function: return functionSlots;
+            case ItemType.Quest: return questSlots;
+        }
+        return null;
     }
 
     private void PutSlot(Slot[] _slots, Item _item, int _count)
     {
-        if (Item.ItemType.Equipment != _item.itemType)
+        if (Item.ItemType.Equipment != _item.itemType) // 장비 아닌 경우 스택 시도
         {
             for (int i = 0; i < _slots.Length; i++)
             {
-                if (_slots[i] != null && _slots[i].item != null)
+                if (_slots[i].item != null && _slots[i].item.itemName == _item.itemName)
                 {
-                    if (_slots[i].item.itemName == _item.itemName)
-                    {
-                        _slots[i].SetSlotCount(_count);
-                        isNotPut = false;
-                        return;
-                    }
+                    _slots[i].SetSlotCount(_count);
+                    isNotPut = false;
+                    return;
                 }
             }
         }
+        // 빈 슬롯 찾기
         for (int i = 0; i < _slots.Length; i++)
         {
-            if (_slots[i] != null && _slots[i].item == null)
+            if (_slots[i].item == null)
             {
                 _slots[i].AddItem(_item, _count);
                 isNotPut = false;
@@ -106,5 +143,9 @@ public class Inventory : MonoBehaviour
 
         isNotPut = true;
     }
-
+    public void OnClickEquipmentTab() => ChangeTab(ItemType.Equipment);
+    public void OnClickUsedTab() => ChangeTab(ItemType.Used);
+    public void OnClickIngredient() => ChangeTab(ItemType.Ingredient);
+    public void OnClickFunctionTab() => ChangeTab(ItemType.Function);
+    public void OnClickQuestTab() => ChangeTab(ItemType.Quest);
 }
