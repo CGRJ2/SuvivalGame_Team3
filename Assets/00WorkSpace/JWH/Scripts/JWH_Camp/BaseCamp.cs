0370200512abf6c79c8dda22936d4af55c0c0354
조원후 <purplehaze0@naver.com>
2025-06-25 11:53:50 +0900

    [Test] BaseCamp_UPgrade Prefabs

--- Assets/00WorkSpace/JWH/Scripts/JWH_Camp/BaseCamp.cs
+++ Assets/00WorkSpace/JWH/Scripts/JWH_Camp/BaseCamp.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCamp : MonoBehaviour
+public class BaseCamp : MonoBehaviour, IInteractable
 {
     public CampLevel camplevel;
     public CampUnlock campunlock;
@@ -14,5 +14,16 @@ public class BaseCamp : MonoBehaviour
             campunlock.UnlockItems(camplevel.CurrentLevel);
             Debug.Log("캠프 업그레이드");
         }
+    }
+
+    public void Interact()
+    {
+        UpgradeCamp();
+    }
+
+    public void SetInteractableEnable()
+    {
+        Debug.Log("캠프 상호작용");
+        
     }
 }
