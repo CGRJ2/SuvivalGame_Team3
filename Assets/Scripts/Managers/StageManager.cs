7721893e928682348e94efe8496bc74db27ae19b
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-07-03 02:13:38 +0900

    [Feat] 스폰 시스템(파밍오브젝트 & 몬스터) 구현
    - 맵 안에 Spanwer들 중 랜덤으로 선택하여 스폰 가능한 시스템 구현
    - 맵 안 오브젝트 최대 수량를 제한하는 기능 추가
    - ActiveOOOList에서 현재 스폰 된 모든 오브젝트에 접근 가능
    - 스폰 된 오브젝트는 비활성화(or 파괴, 오브젝트풀 패턴 적용하여 비활성화로 진행할 예정) 시 해당 오브젝트가 종속되어 있던 ActiveOOOList에서 자동 삭제
    - 해당 아이템이 종속된 ActiveOOOList에서 삭제하는 기능은 ISpawnable 인터페이스를 상속받아야 실행 가능

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -5,7 +5,59 @@ using UnityEngine;
 public class StageManager : Singleton<StageManager>
 {
     [HideInInspector] public StageData[] stageDatas;
+    [Header("현재 스테이지 단계")]
     public ObservableProperty<int> CurrentStageLevel = new ObservableProperty<int>();
+
+
+    [Header("거실 스포너 정보")]
+    [SerializeField] private SpawnerListGroup spawnerListGroup_LivingRoom;
+    [Header("서재 스포너 정보")]
+    [SerializeField] private SpawnerListGroup spawnerListGroup_Library;
+    [Header("옷장 스포너 정보")]
+    [SerializeField] private SpawnerListGroup spawnerListGroup_DressRoom;
+    [Header("안방 스포너 정보")]
+    [SerializeField] private SpawnerListGroup spawnerListGroup_MasterBedRoom;
+
+    public SpawnerListGroup GetSpawnerListGroup(StageKey stageKey)
+    {
+        switch (stageKey)
+        {
+            case StageKey.거실:
+                return spawnerListGroup_LivingRoom;
+            case StageKey.서재:
+                return spawnerListGroup_Library;
+            case StageKey.옷방:
+                return spawnerListGroup_DressRoom;
+            case StageKey.안방:
+                return spawnerListGroup_MasterBedRoom;
+            default: return null;
+        }
+    }
+    //////////////////////테스트/////////////////////////////////
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SpawnCycleTest(spawnerListGroup_LivingRoom);
+        }
+    }
+    //////////////////////테스트/////////////////////////////////
+
+    public void SpawnCycleTest(SpawnerListGroup targetSpawnerGroup)
+    {
+        // FO인스턴스 수량 관리
+        // 최대 수량보다 적게 생성되어 있다면
+        if (targetSpawnerGroup.activedFOInstances.Count < targetSpawnerGroup.maxCount_FO)
+        {
+            // 스포너들 중 랜덤 선택
+            int r = UnityEngine.Random.Range(0, targetSpawnerGroup.spawnerList_FO.Count - 1);
+            targetSpawnerGroup.spawnerList_FO[r].Spawn();
+        }
+        else
+        {
+            Debug.Log("현재 맵 상에 최대 수량으로 존재! 추가 불가");
+        }
+    }
 
 
     public void Init()
@@ -13,12 +65,7 @@ public class StageManager : Singleton<StageManager>
         base.SingletonInit();
 
         // 스테이지 정보 초기화
-        InitStageDatas(); 
-        
-
-        // 스테이지 정보 불러오기 & 클리어 정보 => 현재 스테이지 레벨에 동기화
-        //LoadStageUnlockSaveData(데이터 매니저에서 선택된 세이브 데이터 안에 있는 스테이지언락여부 리스트 받아오기)
-
+        InitStageDatas();
     }
 
     private void OnDestroy()
@@ -26,7 +73,7 @@ public class StageManager : Singleton<StageManager>
         CurrentStageLevel.UnsbscribeAll();
 
     }
-    
+
     // 스테이지 데이터들 초기화하기
     private void InitStageDatas()
     {
@@ -48,7 +95,7 @@ public class StageManager : Singleton<StageManager>
 
 
 
-    #region [세이브 & 로드 데이터]
+    #region [세이브 & 로드 데이터] 1. 스테이지 언락 여부 2. 스포너 상태 및 소환중인 오브젝트 정보
 
     public List<bool> GetStageUnlockSaveData()
     {
@@ -78,4 +125,36 @@ public enum StageKey
     거실, 서재, 옷방, 안방, All
 }
 
+[System.Serializable]
+public class SpawnerListGroup
+{
+    [Header("현재 지역에 존재 가능한 파밍오브젝트 최대 갯수")]
+    public int maxCount_FO = 10;
+    public List<Spawner> spawnerList_FO = new List<Spawner>();
+    public List<GameObject> activedFOInstances = new List<GameObject>();
 
+    [Header("현재 지역에 존재 가능한 몬스터 최대 마릿 수")]
+    public int maxCount_Monster = 10;
+    public List<Spawner> spawnerList_Monster = new List<Spawner>();
+    public List<GameObject> activedMonsterInstances = new List<GameObject>();
+
+    public void ActivedInstancesDataInit()
+    {
+        activedFOInstances = new List<GameObject>();
+        activedMonsterInstances = new List<GameObject>();
+    }
+
+    public void AddToSpawnerList(Spawner spawner)
+    {
+        if (spawner.spawnerType == SpawnerType.FO)
+        {
+            spawnerList_FO.Add(spawner);
+        }
+        else if (spawner.spawnerType == SpawnerType.Monster)
+        {
+            spawnerList_Monster.Add(spawner);
+        }
+    }
+
+
+}
