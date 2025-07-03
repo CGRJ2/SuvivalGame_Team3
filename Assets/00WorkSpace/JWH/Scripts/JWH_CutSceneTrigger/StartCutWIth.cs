7d8cf83ae78108d450219149c0de8964aac74932
조원후 <purplehaze0@naver.com>
2025-07-03 17:25:32 +0900

    [Test] 임시 컷씬 트리거

---
+++ Assets/00WorkSpace/JWH/Scripts/JWH_CutSceneTrigger/StartCutWIth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public void StartCutsceneWith(MonoBehaviour target)
+//{
+//    int rand = Random.Range(0, 2);
+
+//    view.PlayCutscene(rand);
+
+//    // 대상에게 컷씬타입 전달
+//    // 대상은 PlayCutsceneAnim메서드를 가져야함
+//    var method = target.GetType().GetMethod("PlayCutsceneAnim");
+//    if (method != null)
+//        method.Invoke(target, new object[] { rand });
+//}
+
+//플레이어 넣을 스크립트
