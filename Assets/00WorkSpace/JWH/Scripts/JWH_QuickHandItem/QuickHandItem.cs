e87e0a6c9cf76d556038edacc682dd625429930d
조원후 <purplehaze0@naver.com>
2025-07-03 11:37:17 +0900

    [Feat] 퀵슬롯 장비위치 설정하기
    프리팹마다 grip위치를 별도설정 가능
    grip이 없다면 설정한 기본위치를 잡음

--- Assets/00WorkSpace/JWH/Scripts/JWH_QuickHandItem/QuickHandItem.cs
+++ Assets/00WorkSpace/JWH/Scripts/JWH_QuickHandItem/QuickHandItem.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class QuickHandItem : MonoBehaviour
 {
-    [SerializeField] private PlayerStatus playerStatus;
-    [SerializeField] private Transform handTransform;
 
+    [SerializeField] private Transform handTransform;// 손위치 변경시 사용 추천위치:B-palm_01_R
+    private PlayerStatus playerStatus;
     private Item lastItem;
     private GameObject curHandObject;
 
     void Update()
     {
+        if (playerStatus == null)
+        {
+            if (PlayerManager.Instance != null && PlayerManager.Instance.instancePlayer != null)
+            {
+                playerStatus = PlayerManager.Instance.instancePlayer.Status;
+            }
+        }
+
         Item curItem = playerStatus.onHandItem;
         if (curItem != lastItem)
         {
@@ -22,34 +30,42 @@ public class QuickHandItem : MonoBehaviour
 
     private void UpdateHandItem(Item item)
     {
-        //if (item != null && item.itemName == "연습무기")//프리팹 탐색-나중에 아이템에서 전용 필드를 추가하는게 가장 쉬운듯함
-        //{
-        //    item.instancePrefab = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>
-        //        ("Assets/00WorkSpace/JWH/Scripts/JWH_QuickHandItem/연습무기.prefab");
-        //}
-
-        // 기존 손 아이템 제거
         if (curHandObject != null)
         {
-            Destroy(curHandObject);
+            Destroy(curHandObject);// 기존 손 아이템 제거
             curHandObject = null;
         }
 
         if (item != null && item.instancePrefab != null)
         {
             curHandObject = Instantiate(item.instancePrefab);
-            curHandObject.transform.SetParent(handTransform);
-            curHandObject.transform.localPosition = Vector3.zero;
-            curHandObject.transform.localRotation = Quaternion.identity;
+            Transform grip = curHandObject.transform.Find("GripPoint");//프리팹마다 Grip라는 transform으로 잡는위치 할당
+            if (grip != null)
+            {
+                curHandObject.transform.SetParent(handTransform, worldPositionStays: false);
+                Vector3 gripOffset = handTransform.position - grip.position;
+                Quaternion gripRotOffset = handTransform.rotation * Quaternion.Inverse(grip.rotation);
+                curHandObject.transform.position += gripOffset;
+                curHandObject.transform.rotation = gripRotOffset * curHandObject.transform.rotation;
+            }
+            else
+            {
+                Debug.LogWarning($"GripPoint가 없음 {item.itemName}");
+                //curHandObject.transform.SetParent(handTransform);
+                //curHandObject.transform.localPosition = Vector3.zero;
+                //curHandObject.transform.localRotation = Quaternion.identity;
+                curHandObject.transform.SetParent(handTransform, worldPositionStays: false);
 
-            
-            var rb = curHandObject.GetComponent<Rigidbody>();// 콜라이더/물리 꺼줌
-            if (rb != null) rb.isKinematic = true;
+                // 프리팹 손위치 기본값 z손에서다리, x손에서어깨, y손에서 아이템 잡는 위치
+                curHandObject.transform.localPosition = new Vector3(-0.05f, -0.2f, 0.07f); // 손잡이 위치
+                curHandObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f); // 회전
+            }
 
-            var col = curHandObject.GetComponent<Collider>();
-            if (col != null) col.enabled = false;
+            //var rb = curHandObject.GetComponent<Rigidbody>();//물리제거
+            //if (rb != null) rb.isKinematic = true;
+            //var col = curHandObject.GetComponent<Collider>();
+            //if (col != null) col.enabled = false;
 
-           
             var instance = curHandObject.GetComponent<ItemInstance>();
             instance?.InitInstance(item, 1);
         }
